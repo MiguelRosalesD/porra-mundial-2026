7fdb2d0366fa8d139a7fb15b55afecffb8b6e3d8
MiguelRosalesD <mrosalesd99@gmail.com>
2026-06-13 14:26:51 +0200

    Add Fran's predictions (72/72 group + 32 knockout)

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Add each participant's JSON file path here when you receive their predictions
 const PARTICIPANT_FILES = [
   'participants/miguel.json',
-  // 'participants/amigo2.json',
+  'participants/fran.json',
   // 'participants/amigo3.json',
   // 'participants/amigo4.json',
 ];
